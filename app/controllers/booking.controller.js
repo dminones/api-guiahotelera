772ff307c6027ca7d45c9a7a850820c0f75557d3
Your Name <dminones@gmail.com>
2019-01-18 20:52:48 -0300

    Fixging booking message controller

--- app/controllers/booking.controller.js
+++ app/controllers/booking.controller.js
@@ -1,42 +1,41 @@
 import BaseController from './base.controller';
 import nodemailer from 'nodemailer';
-import config from '../config/constants'
+import config from '../config/constants';
 
-const transporter = nodemailer.createTransport(config.smtpConfig)
+const transporter = nodemailer.createTransport(config.smtpConfig);
 
 class BookingController extends BaseController {
-	
-	book = async(req, res, next) => {
-		const site = config.getSite(req.params.site);
-		console.log("SITE -> ", site);
-	    const formData = req.body;
-	    var message = {
-	        from: 'consultas@guiaohoteleraargentina.com',
-	        to: formData.to,
-	        subject: 'Consulta de '+site.name,
-	        text: `
-			Ha recibido la siguiente consulta de ${site.name}
-			${site.url}
-	        --------------------------------------------------------
-	        Nombre: ${formData.name}
-	        Email:  ${formData.email}
-	        Asunto:  ${formData.subject}
-	        Consulta:  ${formData.message}
-	      `,
-	    };
-	    transporter.sendMail(message, (error) => {
-	        if (error) {
-				console.log(error);
-	            res.json({
-	                error: "No pudo enviarse el mensaje. Por favor intente más tarde"
-	            })
-	        } else {
-	            res.json({
-	                response: "Mensaje enviado con exito"
-	            })
-	        }
-	    });
-	}
+  book = async (req, res, next) => {
+    const site = config.getSite(req.params.site);
+    const formData = req.body;
+    const message = {
+      from: 'consultas@guiaohoteleraargentina.com',
+      to: formData.to,
+      subject: 'Consulta de ' + site.name,
+      text: `
+      ${formData.itemName ? `${formData.itemName}. ` : ''}
+      Ha recibido la siguiente consulta de ${site.name}
+      ${formData.url}
+      --------------------------------------------------------        
+      Nombre: ${formData.name}
+      Email: ${formData.email}
+      Asunto: ${formData.subject}
+      Consulta: ${formData.message}
+      `,
+    };
+    transporter.sendMail(message, (error) => {
+      if (error) {
+        console.log(error);
+        res.json({
+          error: 'No pudo enviarse el mensaje. Por favor intente más tarde',
+        });
+      } else {
+        res.json({
+          response: 'Mensaje enviado con exito',
+        });
+      }
+    });
+  };
 }
 
 export default new BookingController();
