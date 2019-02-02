a81bba75af5435e9f16b356e30f90a418187a958
Your Name <dminones@gmail.com>
2019-02-02 19:41:41 -0300

    update url action

--- app/middleware/forest-admin.js
+++ app/middleware/forest-admin.js
@@ -1,9 +1,23 @@
 import ForestAdmin from 'forest-express-mongoose';
+import Destination from '../models/destination';
 
 const addSlug = (req, res, next) => {
   // Your business logic here. 
   next();
 };
+
+const updateUrls = async (req, res, next) => {
+  const items = await Destination.find({ image: /.*http:\/\/www\.guiahoteleraargentina\.com\.*/ });
+  //const result = await Destination.find({}, {$unset: { slug: '' }});
+  const data = []
+  for(let j = 0; j < items.length; j++){
+    const item = items[j]
+    const image = item.image.replace('http://www.guiahoteleraargentina.com/','https://www.guiahoteleraargentina.com/')
+    const result = await Destination.update({_id:item._id}, {$set: { image }});
+    data.push(result)
+  }
+  res.send( { data } );
+}
 
 export default function forestAdmin(app, database) {
   // Setup the Forest Liana middleware in your app.js file
@@ -16,6 +30,12 @@ export default function forestAdmin(app, database) {
     authSecret: process.env.FOREST_AUTH_SECRET,
     mongoose: database, // The mongoose database connection.
   });
+
+  ForestAdmin.collection('Destination', {
+    actions: [{ name: 'Update urls', global: true }],
+  });
+
+  app.post('/forest/actions/update-urls', ForestAdmin.ensureAuthenticated, updateUrls);
 
   app.use(forestAdmin);
 }
